f574ab246bdd8b7d48e5172f041e82241d1a4f03
Chan <Chan@Chan>
2021-12-09 21:18:55 +0530

    CHan2 has made a change

--- src/test/java/com/crm/SDETPracticeConflict/PracticeConflict.java
+++ src/test/java/com/crm/SDETPracticeConflict/PracticeConflict.java
@@ -11,7 +11,7 @@ public class PracticeConflict {
 		System.out.println("COnflict--2");
 		System.out.println("COnflict--3");
 		System.out.println("COnflict--4");
-		System.out.println("");
+		System.out.println("Chanhasmade a change");
 		
 	}
 
