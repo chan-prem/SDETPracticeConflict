daacced00a8043e33cfbfdf8dbf2b3794818bdad
Chan <Chan@Chan>
2021-12-09 21:03:35 +0530

    Prem has created a class.

---
+++ src/test/java/com/crm/SDETPracticeConflict/PracticeConflict.java
@@ -0,0 +1,17 @@
+package com.crm.SDETPracticeConflict;
+
+import org.testng.annotations.Test;
+
+public class PracticeConflict {
+	
+	@Test
+	public void practiceConflict() {
+		
+		System.out.println("COnflict--1");
+		System.out.println("COnflict--2");
+		System.out.println("COnflict--3");
+		System.out.println("COnflict--4");
+		
+	}
+
+}
