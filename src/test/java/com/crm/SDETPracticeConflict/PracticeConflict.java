21792256fb8414558935cb9ffc4fae2daa4570e8
Chan <Chan@Chan>
2021-12-09 21:49:47 +0530

    new

--- src/test/java/com/crm/SDETPracticeConflict/PracticeConflict.java
+++ src/test/java/com/crm/SDETPracticeConflict/PracticeConflict.java
@@ -7,12 +7,8 @@ public class PracticeConflict {
 	@Test
 	public void practiceConflict() {
 		
-		System.out.println("Prem is making a change");
+		System.out.println("col is making a change");
 		
-		System.out.println("COllaborator has made a change");
 		
-		System.out.println("COllaborator has made a change");
-		
-		System.out.println("Prem has made second change");
 	}
 }
