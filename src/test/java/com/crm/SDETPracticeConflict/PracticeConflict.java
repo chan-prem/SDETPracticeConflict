67468e5d9391368bdd0847544916168d2cfebca3
Chan <Chan@Chan>
2021-12-09 21:27:11 +0530

    Prem is updating

--- src/test/java/com/crm/SDETPracticeConflict/PracticeConflict.java
+++ src/test/java/com/crm/SDETPracticeConflict/PracticeConflict.java
@@ -10,8 +10,10 @@ public class PracticeConflict {
 		System.out.println("COnflict--1");
 		System.out.println("COnflict--2");
 		System.out.println("COnflict--3");
+		
 		System.out.println("COnflict--4");
 		System.out.println("nope prem has made the change");
+		
 		
 	}
 
