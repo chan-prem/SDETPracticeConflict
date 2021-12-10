e4be3265880a42bc749d2206a6dc7b2c17294468
Chan <Chan@Chan>
2021-12-10 10:04:31 +0530

    branching

--- src/test/java/com/crm/SDETPracticeConflict/Chekc.java
+++ src/test/java/com/crm/SDETPracticeConflict/Chekc.java
@@ -7,7 +7,7 @@ public class Chekc {
 	
 	@Test
 	public void checkk() {
-		System.out.println("nope changed by chan2");
+		System.out.println("nopfe changed by chan2");
 		System.out.println("10-12");
 	}
 }
