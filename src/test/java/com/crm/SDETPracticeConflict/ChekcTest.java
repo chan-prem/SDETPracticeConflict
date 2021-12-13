acd0d84103dd871e77cbadd7860c38f4010c1bb5
Chan <Chan@Chan>
2021-12-13 12:51:52 +0530

    COmmit

---
+++ src/test/java/com/crm/SDETPracticeConflict/ChekcTest.java
@@ -0,0 +1,15 @@
+package com.crm.SDETPracticeConflict;
+
+import org.testng.annotations.Test;
+
+public class ChekcTest {
+
+	
+	@Test
+	public void checkk() {
+		System.out.println("nopiue changed by chan2");
+
+		System.out.println("nopfe changed by chan2");
+		System.out.println("10-12");
+	}
+}
---
+++ src/test/java/com/crm/SDETPracticeConflict/PracticeConflictTest.java
@@ -0,0 +1,13 @@
+package com.crm.SDETPracticeConflict;
+
+import org.testng.annotations.Test;
+
+public class PracticeConflictTest {
+	
+	@Test
+	public void practiceConflict() {
+		
+		System.out.println("LIamPaine ");
+		
+	}
+}
